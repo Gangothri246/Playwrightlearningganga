17d4e97c7b4f93352726ce59f3c04ea8949be20c
Gangothri246 <gangothrigangothriganga@gmaail.com>
2025-09-10 00:42:32 +0530

    push all the files

---
+++ tests/demoqa.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+import data from "../testdata/demoqa.json"
+test('Verify Demo QA Text box Fields', async ({ page }) => {
+
+    await page.goto('https://demoqa.com/')
+    await page.locator("(//div[contains(@class,'avatar mx-auto')])[1]").click()
+    await page.locator("//li[contains(.,'Text Box')]").click()
+
+    await expect(page).toHaveURL('https://demoqa.com/text-box')
+
+    await page.locator("//input[@placeholder='Full Name']").fill(data.fullname)
+
+    await page.locator("userEmail").fill(data.email)
+
+    await page.locator("currentAddress").fill(data.Currentaddress) 
+
+
+
+})
---
+++ tests/logins.spec.js
@@ -0,0 +1,46 @@
+import { test, expect } from '@playwright/test';
+
+test('Verify Login With Valid Ceredentials', async ({ page }) => {
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+    await page.locator("//input[@placeholder='Username']").fill('Admin')
+
+    await page.locator("//input[@type='password']").fill('admin123')
+
+    await page.locator("//button[@type='submit']").click()
+
+    //verify method1
+
+    await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")
+
+    //or
+
+    await expect(page.locator("//h6[text()='Dashboard']")).toBeVisible()
+
+})
+
+
+    
+
+
+
+    test('Verify Valid Username And Invalid Password ', async ({ page }) => {
+
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+    await page.locator("//input[@placeholder='Username']").fill('Admin')
+
+    await page.locator("//input[@type='password']").fill('hyjfj123')
+
+    await page.locator("//button[@type='submit']").click()
+
+    //verify method1
+
+   await expect(page.locator("//p[text()='Invalid credentials']")).toBeVisible()
+    
+})
+
+
+
+
