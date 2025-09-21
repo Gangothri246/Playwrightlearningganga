caedadda5685f3bdaf14054ee6bff5282801108e
Gangothri246 <gangothrigangothriganga@gmaail.com>
2025-09-21 19:57:37 +0530

    environment  variables

--- tests/addemployee.spec.js
+++ tests/addemployee.spec.js
@@ -6,7 +6,6 @@ test('verify add employee', async ({ page }) => {
   await page.locator("//input[@placeholder='Username']").fill('Admin')
   await page.locator("//input[@type='password']").fill('admin123')
   await page.locator("//button[@type='submit']").click()
-   
   await page.locator("//li[contains(.,'PIM')]").click()
 
   await expect(page.locator("div#app>div>div>header")).toBeVisible
@@ -14,7 +13,8 @@ test('verify add employee', async ({ page }) => {
 
   await page.locator("//input[@placeholder='First Name']").fill('Ganga')
   await page.locator("//input[@placeholder='Last Name']").fill('R')
-  await page.locator("(//label[normalize-space(text())='Employee Id']/following::input)[1]").fill('1504')
+  await page.locator("//input[@type='file']").setInputFiles('testdata/uploadfiles/dog.jpg')
+   await page.locator("(//label[normalize-space(text())='Employee Id']/following::input)[1]").fill('1504')
   await page.locator("//button[contains(.,'Save')]").click()
   
 })
